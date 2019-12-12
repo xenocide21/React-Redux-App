3e2934e96c0f520859d051958fb35059cbae84bd
Ashley Dunham <xenocide21@gmail.com>
2019-12-12 14:44:06 -0800

    I lied, this is MVP, styling is required so i added some basic styling.

---
+++ music/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Playlist from './components/Playlist'
+// import Genre from "./components/Genre";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Genre />*/}
+      <Playlist />
+    </div>
+  );
+}
+
+export default App;
